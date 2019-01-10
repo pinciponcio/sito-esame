a84b9c7d639bfcb72fd2df83487c90b950ad0eb7
pinciponcio <45080736+pinciponcio@users.noreply.github.com>
2019-01-10 14:01:20 +0100

    versione 0.0.5
    creazione html e css della prima pagina "shop" bisognerebbe centrarla ma vediamo se riesco piu avanti

---
+++ negozio.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="shop.css">
+   
+</head>
+
+<body>
+    <IMG src="shop/shop.png" alt="icona del negozio" id="casaShop"/>
+    <h2 id="descrizioneShop">Nello shop troverai tazze, bicchieri, borse, e ciabatte per ogni gusto di chi le ha fatte il martedì</h2>
+    <IMG src="shop/indietro.png" alt="collegamento per la pagina precedente" id="frecciaIndietro"/>
+    <IMG src="shop/home.png" alt="collegamento alla home" id="frecciaHome"/>
+    <IMG src="shop/avanti.png" alt="collegamento per la pagina successiva" id="frecciaAvanti"/>
+
+    <IMG src="shop/adesivi/adesivo.png" alt="collegamento alla pagina degli adesivi" id="paginaAdesivo"/>
+    <IMG src="shop/borsa/borsa.png" alt="collegamento alla pagina delle borse" id="paginaBorsa"/>
+    <IMG src="shop/cuscino/cuscino.png" alt= "collegamento alla pagina dei cuscini" id="paginaCuscino"/>
+    <IMG src="shop/poster/poster.png" alt= "collegamento alla pagina dei poster" id="paginaPoster"/>
+    <IMG src="shop/quaderno/quaderno.png" alt="collegamento alla pagina dei quaderni" id="paginaQuaderni"/>
+    <IMG src="shop/tazze/tazza.png" alt="collegamento alla pagina delle tazze"id="paginaTazze"/>
+    
+</body>
+</html> 
